c4c06df7435d526c182e8d12328c33401e82192a
teonatsaavaaa-art <teonatsaavaaa@gmail.com>
2025-12-18 10:54:48 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        int[] scores = {60, 65, 70, 76, 88,  95};
+        int[] scores = {60, 65, 70, 76, 88, 95};
 
         for (int i = 0; i < scores.length; i++) {
             if (scores[i] > 75) {
