ab7440608f505cebabfeb6ec3656ed35d90dda7c
teonatsaavaaa-art <teonatsaavaaa@gmail.com>
2025-12-18 10:45:55 +0100

    secondo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        int[] scores = {60, 65, 70, 76, 88, 75, 95};
+
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > 75) {
+                System.out.println(scores[i]);
+            }
+        }
+    }
+}
+
+
